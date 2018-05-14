9cb8d9a2695431ec7c159d24fc047994f369d32f
qicaitiankong <zihaoshuhan@163.com>
2018-05-14 22:11:26 +0800

    有添加了一个branch2文件

---
+++ lzhlzhGitTest/branch2.h
@@ -0,0 +1,13 @@
+//
+//  branch2.h
+//  lzhlzhGitTest
+//
+//  Created by 李自豪 on 2018/5/14.
+//  Copyright © 2018年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface branch2 : NSString
+
+@end
---
+++ lzhlzhGitTest/branch2.m
@@ -0,0 +1,13 @@
+//
+//  branch2.m
+//  lzhlzhGitTest
+//
+//  Created by 李自豪 on 2018/5/14.
+//  Copyright © 2018年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import "branch2.h"
+
+@implementation branch2
+
+@end

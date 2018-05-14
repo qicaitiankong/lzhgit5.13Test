ee9b40fb831f7232179898a8f99fb726160b953e
qicaitiankong <zihaoshuhan@163.com>
2018-05-14 21:55:36 +0800

    添加了测试文件

---
+++ lzhlzhGitTest/branchTest.h
@@ -0,0 +1,13 @@
+//
+//  branchTest.h
+//  lzhlzhGitTest
+//
+//  Created by 李自豪 on 2018/5/14.
+//  Copyright © 2018年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface branchTest : NSString
+
+@end
---
+++ lzhlzhGitTest/branchTest.m
@@ -0,0 +1,13 @@
+//
+//  branchTest.m
+//  lzhlzhGitTest
+//
+//  Created by 李自豪 on 2018/5/14.
+//  Copyright © 2018年 haichuanhuiwulianxinxi. All rights reserved.
+//
+
+#import "branchTest.h"
+
+@implementation branchTest
+
+@end
